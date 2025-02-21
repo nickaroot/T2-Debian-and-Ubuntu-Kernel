957c040e6af2d5cb7ed330dce9ec9fad678a0379
Nikita Arutiunov <nickaroot@icloud.com>
2025-02-22 00:35:12 +0300

    6.8.12 tryout

--- patch_driver.sh
+++ patch_driver.sh
@@ -5,8 +5,8 @@ set -eu -o pipefail
 BUILD_PATH=/tmp/build-kernel
 
 # Patches
-T2_PATCHES_GIT_URL=https://github.com/t2linux/linux-t2-patches.git
-T2_PATCHES_BRANCH_NAME=6.12
+T2_PATCHES_GIT_URL=https://github.com/nickaroot/linux-t2-patches.git
+T2_PATCHES_BRANCH_NAME=6.8
 T2_PATCHES_COMMIT_HASH=HEAD
 
 rm -rf "${BUILD_PATH}"
